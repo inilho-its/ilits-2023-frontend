815cff9730590797d197815acde12c2a6f3abbb3
ahnafms <ahnafmusyaffa47@gmail.com>
2022-10-27 21:10:57 +0700

    feat:update coming soon page

--- src/pages/coming-soon.tsx
+++ src/pages/coming-soon.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import * as React from 'react';
 
 import Layout from '@/components/layout/Layout';
@@ -7,77 +8,28 @@ export default function CommingsoonPage() {
   return (
     <Layout>
       <Seo templateTitle='Coming-Soon' />
-
       <main>
         <section className='Coming Soon'>
-          <div className='relative flex min-h-screen items-center justify-center overflow-hidden bg-gray-900'>
-            {/* <!-- Grid background --> */}
-            <div className='grid-background absolute inset-0 grid -skew-y-12 scale-150 transform grid-cols-12 gap-2 p-2'>
-              {/* <!-- row 1 --> */}
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-5 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-1 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-4 animate-pulse rounded bg-gray-800'></div>
-              {/* <!-- row 2 --> */}
-              <div className='col-span-5 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-3 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              {/* <!-- row 3 --> */}
-              <div className='col-span-4 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-7 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-1 animate-pulse rounded bg-gray-800'></div>
-
-              {/* <!-- row 4 --> */}
-              <div className='col-span-4 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              {/* <!-- row 5 --> */}
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-5 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-1 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-4 animate-pulse rounded bg-gray-800'></div>
-
-              {/* <!-- row 6 --> */}
-              <div className='col-span-4 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-7 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-1 animate-pulse rounded bg-gray-800'></div>
-
-              <div className='col-span-5 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-1 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-3 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-3 animate-pulse rounded bg-gray-800'></div>
-
-              <div className='col-span-2 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-5 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-1 animate-pulse rounded bg-gray-800'></div>
-              <div className='col-span-4 animate-pulse rounded bg-gray-800'></div>
+          <div className='relative flex min-h-screen w-full overflow-hidden bg-[#F2F2F2] '>
+            <div className='absolute top-4 left-[-20px] h-[282px] w-[227.38px] blur-lg sm:top-0 sm:left-[0px] sm:h-[350px] sm:w-[350px] sm:blur-none  lg:left-0 lg:h-[550px] lg:w-[375px]'>
+              <Image alt='/' src='/images/bg-update1.svg' layout='fill' />
             </div>
-
-            <div className='relative'>
-              <h2 className='flex flex-row items-center text-4xl font-bold text-white md:text-8xl'>
-                Coming
-                <div className='relative mx-2 text-sm'>
-                  <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    width='16'
-                    height='16'
-                    fill='currentColor'
-                    className='bi bi-caret-up-fill text-blue-500'
-                    viewBox='0 0 16 16'
-                  >
-                    <path d='m7.247 4.86-4.796 5.481c-.566.647-.106 1.659.753 1.659h9.592a1 1 0 0 0 .753-1.659l-4.796-5.48a1 1 0 0 0-1.506 0z' />
-                  </svg>
-                  <div className='absolute -top-12 -rotate-45 transform text-blue-500'>
-                    <p className='rounded-md bg-blue-500 px-2 py-0 text-base font-light text-white'>
-                      super
-                    </p>
-                  </div>
-                </div>
-                Soon
-              </h2>
+            <div className='absolute bottom-0 -right-[30px] h-[250px] w-[200px] blur-lg sm:right-20 sm:blur-none lg:h-[287px] lg:w-[239px]'>
+              <Image alt='/' src='/images/bg-update2.svg' layout='fill' />
+            </div>
+            <div className='m-auto'>
+              <p className='text-center text-[39.1px] font-semibold text-[#3B71C7] sm:text-[44px] lg:text-6xl'>
+                Coming Soon
+              </p>
+              <p className='pt-5 text-center text-lg font-[600] text-[#3B71C7] sm:text-xl lg:pt-8 lg:text-3xl'>
+                <span className='bg-gradient-to-r from-[#487DBC] to-[#60BEE0] bg-clip-text pr-[7px] font-[600] text-transparent'>
+                  INI LHO ITS! 2023
+                </span>
+                akan segera hadir
+              </p>
+              <p className='text-center text-lg font-[600] text-[#3B71C7] sm:text-xl md:pt-2  lg:pt-3 lg:text-3xl'>
+                Stay tune.
+              </p>
             </div>
           </div>
         </section>
