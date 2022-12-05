08e3538fbd65278791fee87af13ee435d0df070d
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-12-05 16:54:31 +0700

    refactor: fixing layout component & add dialog

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,19 +1,40 @@
 import * as React from 'react';
 
+import BaseDialog from '@/components/dialog/BaseDialog';
 import Footer from '@/components/layout/Footer';
 import Navbar from '@/components/layout/Navbar';
 
+import useDialogStore from '@/store/useDialogStore';
+
 type LayoutPros = {
   children: React.ReactNode;
-};
+  withFooter?: boolean;
+  withNavbar?: boolean;
+} & React.ComponentPropsWithRef<'div'>;
 
-export default function Layout({ children }: LayoutPros) {
-  // Put Header or Footer Here
+export default function Layout({
+  children,
+  withFooter = true,
+  withNavbar = true,
+}: LayoutPros) {
+  //#region  //*=========== Store ===========
+  const open = useDialogStore.useOpen();
+  const state = useDialogStore.useState();
+  const handleClose = useDialogStore.useHandleClose();
+  const handleSubmit = useDialogStore.useHandleSubmit();
+  //#endregion  //*======== Store ===========
+
   return (
     <div className='overflow-x-hidden bg-bone-100'>
-      <Navbar />
+      {withNavbar && <Navbar />}
       {children}
-      <Footer />
+      <BaseDialog
+        onClose={handleClose}
+        onSubmit={handleSubmit}
+        open={open}
+        options={state}
+      />
+      {withFooter && <Footer />}
     </div>
   );
 }
