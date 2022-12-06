d8891e91826b387d7174cf542710591b36c2d1ab
sourLilac <nathanaelpg8@gmail.com>
2022-12-06 20:59:11 +0700

    feat: add content

--- src/constant/homeData.ts
+++ src/constant/homeData.ts
@@ -2,17 +2,17 @@ export const cardPrestasi = [
   {
     src: '/images/homePage/prestasi1.png',
     alt: 'Prestasi ITS 1',
-    label: 'Juara 1 Loncat Ga Turun-turun',
+    label: 'Tak Terkalahkan, ITS Pertahankan Juara Umum KRI Kali Kelima',
   },
   {
     src: '/images/homePage/prestasi2.png',
     alt: 'Prestasi ITS 2',
-    label: 'Juara 1 Loncat Ga Turun-turun',
+    label: 'Sapuangin ITS Borong Empat Penghargaan di Shell Eco-Marathon 2021',
   },
   {
     src: '/images/homePage/prestasi3.png',
     alt: 'Prestasi ITS 3',
-    label: 'Juara 1 Loncat Ga Turun-turun',
+    label: 'Tim Robot ITS Juarai Laga Internasional RoboCup 2022',
   },
 ];
 
@@ -25,40 +25,40 @@ export const alumni = [
   {
     name: 'Cak Lontong',
     job: 'Komedian',
-    src: '/images/homePage/alumni1.png',
+    src: '/images/homePage/alumni2.png',
   },
   {
-    name: 'Tri Rismaharini',
-    job: 'Menteri Sosial',
-    src: '/images/homePage/alumni1.png',
+    name: 'Muhammad Nuh',
+    job: '(ex.) Menteri Pendidikan',
+    src: '/images/homePage/alumni3.png',
   },
   {
-    name: 'Tri Rismaharini',
-    job: 'Menteri Sosial',
-    src: '/images/homePage/alumni1.png',
+    name: 'Dwi Soetjipto',
+    job: '(ex.) Direktur PT. Telkom',
+    src: '/images/homePage/alumni4.png',
   },
 ];
 
 export const beasiswa = [
   {
-    src: '/images/homePage/kemdikbud.png',
+    src: '/images/homePage/beasiswabi.png',
     href: '',
     label: 'Beasiswa Bank Indonesia',
   },
   {
-    src: '/images/homePage/kemdikbud.png',
+    src: '/images/homePage/beasiswadik.png',
     href: '',
     label: 'Beasiswa Unggulan',
   },
   {
-    src: '/images/homePage/kemdikbud.png',
+    src: '/images/homePage/beasiswakse.png',
     href: '',
-    label: 'Beasiswa Bank Indonesia',
+    label: 'Beasiswa KSE',
   },
   {
-    src: '/images/homePage/kemdikbud.png',
+    src: '/images/homePage/beasiswabca.png',
     href: '',
-    label: 'Beasiswa Bank Indonesia',
+    label: 'Beasiswa BCA',
   },
 ];
 
@@ -76,34 +76,44 @@ export const companyLogo = [
 
 export const openCampus = [
   {
-    title: 'Open Campus',
+    title: 'ITS FAIR x FACULTY',
     paragraph:
-      'Pretium id egestas convallis commodo. Ullamcorper sit habitasse non elementum sed. Urna netus nulla at mi sit. Ut placerat eget at pretium et in. Morbi proin sit lectus mattis et. Quisque at convallis sed ut. ',
+      'Kegiatan yang bertujuan untuk mewujudkan penyebarluasan informasi seputar ITS dan semangat meraih pendidikan setinggi-tingginya kepada siswa/siswi SMA sederajat dalam bentuk talkshow yang menghadirkan pembicara yang akan menjelaskan mengenai ITS dan kehidupan mahasiswa di ITS.',
   },
   {
-    title: 'Open Campus',
+    title: 'ILITS WORLDWIDE',
     paragraph:
-      'Pretium id egestas convallis commodo. Ullamcorper sit habitasse non elementum sed. Urna netus nulla at mi sit. Ut placerat eget at pretium et in. Morbi proin sit lectus mattis et. Quisque at convallis sed ut. ',
+      'Kegiatan yang bertujuan untuk mewujudkan tujuan Ini Lho ITS! untuk menyebarluaskan informasi mengenai ITS dengan segmentasi pesertanya adalah siswa/siswi yang bersekolah di SILN dan Siswa/Siswi yang bersekolah di luar negeri. ILITS_WORLDWIDE merupakan Inovasi baru yang merupakan usaha Ini Lho ITS! Dalam menerbang-tinggikan nama Institut Teknologi Sepuluh Nopember ke seluruh dunia.',
   },
   {
-    title: 'Open Campus',
+    title: 'ONE DAY LECTURE',
     paragraph:
-      'Pretium id egestas convallis commodo. Ullamcorper sit habitasse non elementum sed. Urna netus nulla at mi sit. Ut placerat eget at pretium et in. Morbi proin sit lectus mattis et. Quisque at convallis sed ut. ',
+      'Kegiatan yang memberikan pengalaman langsung kepada para peserta untuk bisa mencoba bagaimana menjadi mahasiswa selama satu hari sesuai dengan jurusan yang mereka pilih.',
   },
   {
-    title: 'Open Campus',
+    title: 'WELCOME',
     paragraph:
-      'Pretium id egestas convallis commodo. Ullamcorper sit habitasse non elementum sed. Urna netus nulla at mi sit. Ut placerat eget at pretium et in. Morbi proin sit lectus mattis et. Quisque at convallis sed ut. ',
+      'Kegiatan tahunan Ini Lho ITS! yang diselenggarakan di Surabaya, dan juga di berbagai daerah di Indonesia yang bekerja sama dengan Forum Daerah (Forda) dengan kegiatan utama adalah tryout dan pembahasan soal dan materi persiapan tes masuk perguruan tinggi.',
   },
   {
-    title: 'Open Campus',
+    title: 'TOUR DE CAMPUS',
     paragraph:
-      'Pretium id egestas convallis commodo. Ullamcorper sit habitasse non elementum sed. Urna netus nulla at mi sit. Ut placerat eget at pretium et in. Morbi proin sit lectus mattis et. Quisque at convallis sed ut. ',
+      'Kegiatan yang memberikan pengalaman langsung kepada para peserta untuk bisa lebih mengenal lingkungan ITS khususnya di masing masing fakultas dan departemen.',
   },
   {
-    title: 'Open Campus',
+    title: 'VISIT SURABAYA',
     paragraph:
-      'Pretium id egestas convallis commodo. Ullamcorper sit habitasse non elementum sed. Urna netus nulla at mi sit. Ut placerat eget at pretium et in. Morbi proin sit lectus mattis et. Quisque at convallis sed ut. ',
+      'Kegiatan dalam rangka menyebarluaskan informasi mengenai ITS sekaligus menyebarluaskan semangat perguruan tinggi kepada seluruh siswa/siswi SMA SMK sederajat yang tersebar di seluruh daerah surabaya.',
+  },
+  {
+    title: 'VISIT FORUM DAERAH',
+    paragraph:
+      'Kegiatan dalam rangka menyebarluaskan informasi mengenai ITS sekaligus menyebarluaskan semangat perguruan tinggi negeri kepada seluruh siswa/siswi dari SMA/SMK/sederajat yang tersebar di seluruh daerah di Indonesia. Nantinya panitia Ini Lho ITS! 2023 akan bekerja sama dengan panitia Forum Daerah (Forda) yang tersebar di seluruh daerah di Indonesia untuk mengeksekusi kegiatan ini.',
+  },
+  {
+    title: 'ILITS EXPEDITION',
+    paragraph:
+      'Kegiatan yang menjadi salah satu inovasi dan proyeksi baru di Ini Lho ITS! 2023, dimana Ini Lho ITS akan terjun langsung ke beberapa daerah pelosok yang dinilai masih kurang terkait penyebaran semangat perguruan tinggi negeri dan segala informasi mengenai ITS dan membawa semangat meraih pendidikan dan kesempatan di perguruan tinggi ke daerah tersebut.',
   },
 ];
 
@@ -111,43 +121,50 @@ export const fakultas = [
   {
     id: 1,
     nama: 'Teknologi Industri dan Rekayasa Sistem',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    src: '/',
+    desc: 'Dengan populasi Indonesia yang lebih dari 250 juta dan luasnya mencapai 1.905 juta km2, tentu saja sangat strategis untuk memenuhi kebutuhan mereka dengan menggunakan sumber daya alam yang melimpah. Departemen yang ada di Fakultas Teknologi Industri dan Rekayasa Sistem: Teknik Mesin, Teknik Kimia, Teknik Fisika, Teknik Industri, Teknik Material dan Metalurgi.',
+    src: '/images/homePage/biologi.png',
+    smallsrc: '/images/homePage/fti-mobile.png',
   },
   {
     id: 2,
     nama: 'Teknik Sipil, Perencanaan dan Kebumian',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    src: '/',
+    desc: 'Fakultas Teknik Sipil, Perencanaan, dan Kebumian (Civplan) merupakan hasil perampingan fakultas berdasarkan penyesuaian kelompok ilmu dan bidang ilmu di ITS. Civplan terdiri dari enam jurusan, yaitu Teknik Sipil, Arsitektur, Teknik Lingkungan, Perencanaan Wilayah Kota, Teknik Geomatika, dan Teknik Geofisika.',
+    src: '/images/homePage/ftspk.png',
+    smallsrc: '/images/homePage/ftspk-mobile.png',
   },
   {
     id: 3,
     nama: 'Teknik Elektro dan Informatika Cerdas',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    src: '/',
+    desc: 'F-Electics yang terdiri dari enam departemen serta delapan program studi untuk S1 ini memiliki lebih dari 2500 mahasiswa baik di tingkat S1, S2 ataupun S3 dengan visi membentuk sumber daya manusia berkarakter, berbudi unggul serta berkelas dunia dalam bidang teknologi elektro, sistem informasi, biomedik, komputer, informatika dan teknologi informasi.',
+    src: '/images/homePage/fteic.png',
+    smallsrc: '/images/homePage/fteic-mobile.png',
   },
   {
     id: 4,
     nama: 'Sains dan Analitika Data',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    src: '/',
+    desc: 'Fakultas Sains dan Analitika Data memiliki jejak perjalanan panjang dalam sejarah Kampus Pahlawan. Untuk mendukung visi ITS menjadi universitas bereputasi internasional atau World Class University, serta dalam rangka menjawab tantangan industri 4.0, pada tahun 2020 fakultas ini mendapat nama baru yakni Fakultas Sains dan Analitika Data (FSAD).',
+    src: '/images/homePage/fsad.png',
+    smallsrc: '/images/homePage/fsad-mobile.png',
   },
   {
     id: 5,
     nama: 'Desain Kreatif dan Bisnis Digital',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    src: '/',
+    desc: 'Terdiri dari 6 Departemen: Manajemen Teknologi, Desain Produk, Manajemen Bisnis, Desain Interior, Desain Komunikasi Visual dan Studi Pembangunan, Fakultas CREABIZ mempersiapkan sumber daya unggul yang kreatif, inovatif dan adaptif dengan design thinking dan kemampuan manajerial untuk mengantisipasi kecepatan perubahan teknologi dan informasi untuk kesejahteraan manusia dan alam sekitarnya.',
+    src: '/images/homePage/fbiz.png',
+    smallsrc: '/images/homePage/fbiz-mobile.png',
   },
   {
     id: 6,
     nama: 'Teknologi Kelautan',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    src: '/',
+    desc: 'Berorientasi pada Kemaritiman Indonesia, Fakultas Teknologi Kelautan terus berkomitmen untuk memberikan kontribusi dalam mendukung Indonesia sebagai poros maritim dunia. Di Fakultas Teknologi Kelautan terdapat 4 Departemen, yaitu Teknik perkapalan, Teknik sistem perkapalan, Teknik kelautan, dan teknik Transportasi laut',
+    src: '/images/homePage/fkel.png',
+    smallsrc: '/images/homePage/fkel-mobile.png',
   },
   {
     id: 7,
     nama: 'Vokasi',
-    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-    src: '/',
+    desc: 'Fakultas Vokasi ITS merupakan Fakultas yang memiliki 6 (enam) Departemen yaitu; Teknik Infrastruktur Sipil, Teknik Mesin Industri, Teknik Elektro Otomasi, Teknik Kimia Industri, Teknik Instrumentasi dan Statistika Bisnis dengan 8 (delapan) Program studi Sarjana Terapan dan 1 (satu) Magister Terapan',
+    src: '/images/homePage/fv.png',
+    smallsrc: '/images/homePage/fv-mobile.png',
   },
 ];
