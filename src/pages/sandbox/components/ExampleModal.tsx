6eaffb4b7743768eabf0f750aa4b87f517ca00d1
Robby Pambudi <5025211042@mhs.its.ac.id>
2022-12-31 01:33:03 +0700

    feat: create example modal with less render and big file

--- src/pages/sandbox/components/ExampleModal.tsx
+++ src/pages/sandbox/components/ExampleModal.tsx
@@ -4,44 +4,66 @@ import Button from '@/components/buttons/Button';
 import Modal from '@/components/modal/Modal';
 import Typography from '@/components/typography/Typography';
 
+type DataType = {
+  name: string;
+  image: string;
+};
+
 type ModalReturnType = {
-  openModal: () => void;
+  openModal: (data: DataType) => void;
 };
 
 export default function ExampleModal({
   children,
-  data,
 }: {
   children: (props: ModalReturnType) => JSX.Element;
-  data: {
-    name: string;
-    image: string;
-  };
 }) {
   const [open, setOpen] = React.useState(false);
+  const [state, setState] = React.useState<DataType>();
   const modalReturn: ModalReturnType = {
-    openModal: () => setOpen(true),
+    openModal: (data) => {
+      setState(data);
+      setOpen(true);
+    },
   };
 
   return (
     <>
       {children(modalReturn)}
       <Modal open={open} setOpen={setOpen}>
-        <Modal.Title>{data.name}</Modal.Title>
-        <Modal.Body>
-          <Typography variant='b2'>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-            ex cumque assumenda! Nesciunt reiciendis dolores sit atque incidunt
-            debitis repudiandae.
-          </Typography>
-          <div className='mt-3 grid grid-cols-[1fr,3fr] gap-2'>
-            <Button className='justify-center' variant='bone'>
-              Hi
-            </Button>
-            <Button className='justify-center'>Hello</Button>
-          </div>
-        </Modal.Body>
+        {state && <ModalExample state={state} setOpen={setOpen} />}
       </Modal>
+    </>
+  );
+}
+
+function ModalExample({
+  state,
+  setOpen,
+}: {
+  state: DataType;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
+  return (
+    <>
+      <Modal.Title>{state.name}</Modal.Title>
+      <Modal.Body>
+        <Typography variant='b2'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          ex cumque assumenda! Nesciunt reiciendis dolores sit atque incidunt
+          debitis repudiandae.
+        </Typography>
+        <div className='mt-3 grid grid-cols-[1fr,3fr] gap-2'>
+          <Button
+            className='justify-center'
+            variant='bone'
+            onClick={() => setOpen(false)}
+          >
+            Hi
+          </Button>
+          <Button className='justify-center'>Hello</Button>
+        </div>
+      </Modal.Body>
     </>
   );
 }
