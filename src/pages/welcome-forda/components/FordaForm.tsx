0832a8774c90285873875bb1307cb0c47103f236
Robby Pambudi <34505233+robbypambudi@users.noreply.github.com>
2023-01-17 20:20:50 +0700

    Update FordaForm.tsx

--- src/pages/welcome-forda/components/FordaForm.tsx
+++ src/pages/welcome-forda/components/FordaForm.tsx
@@ -67,8 +67,9 @@ export default function FordaFormPage({ setStep }: FordaFormPageProps) {
               item.nama_forda !== 'Tuban Jawa Timur' &&
               item.nama_forda !== 'Situbondo' &&
               item.nama_forda !== 'Pamekasan' &&
-              item.nama_forda !== 'Madiun'
-              item.nama_forda !== 'Tulungagung'
+              item.nama_forda !== 'Madiun' &&
+              item.nama_forda !== 'Tulungagung' &&
+              item.nama_forda !== 'Bali'
             ) {
               return (
                 <option key={item.id} value={item.id}>
